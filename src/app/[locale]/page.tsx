5decbe235494cd9ee9677c6d95215bbb9f037ffa
mohamed abdelrazek <mohamedbadawy296@gmail.com>
2024-10-27 17:34:50 +0200

    vercel deploy

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,14 +1,14 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Nav from "@/components/Nav";
-import { useTranslations } from "next-intl";
+// import { useTranslations } from "next-intl";
 
 
 
 export default function IndexPage() {
     // Enable static rendering
 
-    const t = useTranslations("HomePage");
+    // const t = useTranslations("HomePage");
 
     return (
             <div>
