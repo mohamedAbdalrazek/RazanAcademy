e01613631d84bfa30185ea7fc8d23ea3892c1208
mohamed abdelrazek <mohamedbadawy296@gmail.com>
2024-11-04 18:38:08 +0200

    finishing blog page with translation, meta data

--- src/app/[locale]/blog/page.tsx
+++ src/app/[locale]/blog/page.tsx
@@ -4,13 +4,30 @@ import SubHeading from "@/components/global/SubHeading";
 import React from "react";
 import styles from "@/styles/blog/Blog.module.css"
 import PostsList from "@/components/blog/PostsList";
-export default function page(){
+import { useTranslations } from "next-intl";
+import { getTranslations } from "next-intl/server";
+type Props = {
+    params: { locale: string };
+};
+
+export async function generateMetadata({
+    params: { locale },
+}: Omit<Props, "children">) {
+    const t = await getTranslations({ locale, namespace: "metaData" });
+
+    return {
+        title: t("BlogPage.title"),
+        description: t("BlogPage.description"),
+    };
+}
+export default function Blog(){
+    const t= useTranslations("Blog")
     return (
         <div className={styles.blog}>
             <Heading text="Blog" />
-            <SubHeading text="Recent Post" />
+            <SubHeading text={t(`recent`)} />
             <RecentPost />
-            <SubHeading text="Discover Posts" />
+            <SubHeading text={t(`discover`)} />
             <PostsList />
         </div>
     );
--- src/components/blog/RecentPost.tsx
+++ src/components/blog/RecentPost.tsx
@@ -3,30 +3,31 @@ import React from "react";
 import styles from "@/styles/blog/RecentPost.module.css";
 import { Link } from "@/i18n/routing";
 import Date from "../global/Date";
+import { useTranslations } from "next-intl";
 export default function RecentPost() {
-                    
-    const title = "The Importance of Tajweed in Quranic Recitation"
+    const t = useTranslations("Blog")
+    const title = t(`recentPost.title`)
+    const src = t(`recentPost.src`)
+    const description = t(`recentPost.description`)
+    const date = t(`recentPost.date`)
     const url = title.split(" ").join("-")
     return (
         <Link href={`/blog/${url}`} className={styles.recentPost}>
             <Image
                 className={styles.image}
-                src={"/images/mockBackground-one-1.jpg"}
+                src={src}
                 width={1000}
                 height={1000}
-                alt="The Importance of Tajweed in Quranic Recitation"
+                alt={title}
             />
             <div className={styles.infoWrapper}>
                 <h3 className={styles.header}>
                     {title}
                 </h3>
                 <p className={styles.description}>
-                    Discove r why mastering Tajweed is essential for accurate
-                    and beautiful Quranic recitation. Learn how proper
-                    pronunciation enhances both the spiritual and linguistic
-                    experience of reading the Holy Quran.
+                    {description}
                 </p>
-                <Date date="Jun 25, 2023" />
+                <Date date={date} />
             </div>
         </Link>
     );
