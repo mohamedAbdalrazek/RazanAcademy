909fc848e920ac3b32e1d32b8d931f5aadb29089
Mohamed Abdelrazek <mohamed.abdelrazek@schoolvoice.com>
2026-03-06 13:35:47 +0200

    adding facebook pixels

--- src/components/BaseLayout.tsx
+++ src/components/BaseLayout.tsx
@@ -9,6 +9,7 @@ import Nav from "./Nav";
 import Footer from "./Footer";
 import WhatsappButton from "./WhatsappButton";
 import { Toaster } from "react-hot-toast";
+import Script from "next/script";
 type Props = {
     children: ReactNode;
     locale: string;
@@ -24,6 +25,25 @@ export default async function BaseLayout({ children, locale }: Props) {
     const messages = await getMessages();
     return (
         <html lang={locale} dir={locale === "ar" ? "rtl" : "ltr"}>
+            <Script
+                id="facebook-pixel"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              
+              fbq('init', '787824725887629');
+              fbq('track', 'PageView');
+            `,
+                }}
+            />
             <body className={`${roboto_mono.className} ${roboto.className}`}>
                 <NextIntlClientProvider messages={messages}>
                     <Header />
